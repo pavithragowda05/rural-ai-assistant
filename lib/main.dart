efaf5b4e7a72d94f31191c5ea2ef4c58c7557815
pavithragowda05 <pavithragowda954@gmail.com>
2026-04-27 22:20:51 +0530

    Added voice feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_to_text.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,18 +11,54 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: VoiceScreen(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class VoiceScreen extends StatefulWidget {
+  @override
+  _VoiceScreenState createState() => _VoiceScreenState();
+}
+
+class _VoiceScreenState extends State<VoiceScreen> {
+  SpeechToText speech = SpeechToText();
+  FlutterTts tts = FlutterTts();
+
+  bool isListening = false;
+  String text = "Press mic and speak";
+
+  void startListening() async {
+    bool available = await speech.initialize();
+    if (available) {
+      setState(() => isListening = true);
+
+      speech.listen(onResult: (result) {
+        setState(() {
+          text = result.recognizedWords;
+        });
+      });
+    }
+  }
+
+  void stopListening() async {
+    await speech.stop();
+    setState(() => isListening = false);
+    speakResponse(text);
+  }
+
+  void speakResponse(String input) async {
+    String response = "You said: $input";
+    setState(() => text = response);
+
+    await tts.speak(response);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Rural AI Assistant"),
-        centerTitle: true,
         backgroundColor: Colors.green,
       ),
       body: Center(
@@ -30,17 +68,15 @@ class HomeScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                "Press mic and speak",
+                text,
                 style: TextStyle(fontSize: 20),
                 textAlign: TextAlign.center,
               ),
               SizedBox(height: 40),
               FloatingActionButton(
-                onPressed: () {
-                  print("Mic pressed");
-                },
+                onPressed: isListening ? stopListening : startListening,
                 backgroundColor: Colors.green,
-                child: Icon(Icons.mic),
+                child: Icon(isListening ? Icons.stop : Icons.mic),
               ),
             ],
           ),
